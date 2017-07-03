15b20159e250eece37ba864d870bd71e7fdcd60f
Devin Mozee <alonzo.mozee@gmail.com>
2017-07-03 17:20:24 -0400

    Change settings.py

--- reverse_job_board/wsgi.py
+++ reverse_job_board/wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "reverse_job_board.settings.local")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "reverse_job_board.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
