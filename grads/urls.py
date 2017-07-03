4c9f02bb0996a7afd8473ed21cc110c53f1734f5
Devin Mozee <alonzo.mozee@gmail.com>
2017-07-03 18:11:05 -0400

    Change urls

--- grads/urls.py
+++ grads/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^/$', views.index, name='grads'),
+    url(r'^$', views.index, name='grads'),
     url(r'^grad/(?P<slug>[\w-]+)/$', views.grad_detail, name='grad_detail'),
     url(r'^profile/(?P<slug>[\w-]+)/$', views.get_profleurl),
     url(r'^edit/$', views.EditProfileView.as_view(), name='grad_profile'),
