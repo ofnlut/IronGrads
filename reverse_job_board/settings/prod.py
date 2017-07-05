4477366ba50d230e9fba0dac63df77c3b9ebac92
Devin Mozee <alonzo.mozee@gmail.com>
2017-07-05 11:30:59 -0400

    Change production settings

--- reverse_job_board/settings/prod.py
+++ reverse_job_board/settings/prod.py
@@ -7,7 +7,7 @@ DEBUG = False
 ALLOWED_HOSTS = ['iron-yard-grads.herokuapp.com','127.0.0.1']
 
 #Secure Middleware Stuff
-
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_SECONDS = 30
 SECURE_SSL_REDIRECT = True
 SECURE_BROWSER_XSS_FILTER = True
