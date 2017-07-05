46371bc4e5fc56811da477273ada91e8a921059a
Devin Mozee <alonzo.mozee@gmail.com>
2017-07-05 12:54:12 -0400

    Change production settings

--- reverse_job_board/settings/prod.py
+++ reverse_job_board/settings/prod.py
@@ -5,3 +5,16 @@ import dj_database_url
 DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com']
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+#Secure Middleware Stuff
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_SECONDS = 10
+SECURE_SSL_REDIRECT = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+X_FRAME_OPTIONS = 'DENY'
+
+#Cookies
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
